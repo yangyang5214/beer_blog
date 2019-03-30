e8b12bee287e1cddb3567d5fdde947cc26b6386d
beer <houhuo5214@126.com>
2019-03-31 00:36:47 +0800

    add photos

--- blog_img.py
+++ blog_img.py
@@ -113,9 +113,9 @@ def cosupload_operation(file_name, im_file_name):
     """
     cosupload 上传到 腾讯cos
     """
-    print("cosupload" + " " + file_name + " " + cos_img + file_name)
-    os.system("cosupload" + " " + file_name + " " + cos_img + file_name)
-    os.system("cosupload" + " " + im_file_name + " " + cos_im_img + file_name)
+    print("sudo cosupload" + " " + file_name + " " + cos_img + file_name)
+    os.system("sudo cosupload" + " " + file_name + " " + cos_img + file_name)
+    os.system("sudo cosupload" + " " + im_file_name + " " + cos_im_img + file_name)
 
 
 def blog_operation():
